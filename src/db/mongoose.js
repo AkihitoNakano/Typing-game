159020f52dc7fb45d30eb44ab3809594a9c943a6
AkihitoNakano <linkin_087@yahoo.co.jp>
2022-05-18 14:02:26 +0900

    move seacret keys to enviroment variable

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose')
-
+require('dotenv').config()
 // データベース接続
 mongoose
-    .connect(
-        'mongodb+srv://resonant:JTXB7ovGguJlcsnP@cluster0.ktf3y.mongodb.net/typing_game?retryWrites=true&w=majority'
-    )
+    .connect(process.env.MONGODB_URL)
     .then(() => console.log('データベース接続に成功しました'))
     .catch(e => console.log(e))
